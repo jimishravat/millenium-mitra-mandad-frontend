28f39be359cabde8d4c90ef2561bc2cf2ac9ebcf
Jimish Ravat <jimishravat2802@gmail.com>
2025-12-27 15:54:01 +0530

    Removed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwwE1TfMwWc3fjtgTMSDw-U28XWKORENM",
-  authDomain: "mandand-b17c7.firebaseapp.com",
-  projectId: "mandand-b17c7",
-  storageBucket: "mandand-b17c7.firebasestorage.app",
-  messagingSenderId: "717189244138",
-  appId: "1:717189244138:web:eaa28b1e58df3244efa393"
+  apiKey: "-",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
